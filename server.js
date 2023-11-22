930d540bcba7ffa52c2224e27ce07ca51af89e49
David L Whitehurst <me@dlwhitehurst.com>
2023-11-22 11:15:48 -0500

    removed proxy middleware, not needed

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const cookieSession = require("cookie-session");
 const app = express();
 
 require('dotenv').config();
-const USERAUTH_SERVER_PORT_URL = process.env.USERAUTH_SERVER_PORT_URL;
 
 app.use(cors());
 
@@ -23,21 +22,10 @@ app.use(
     })
 );
 
-const { createProxyMiddleware } = require('http-proxy-middleware');
-module.exports = function(app) {
-  app.use(
-      '/userauth',
-      createProxyMiddleware({
-        target: USERAUTH_SERVER_PORT_URL,
-        changeOrigin: true,
-      })
-  );
-};
-
 // database
 const db = require("./app/models");
-const Asset = db.prescription;
-const Debt = db.prescription;
+const Asset = db.asset;
+const Debt = db.debt;
 
 /*
 db.sequelize.sync({force: true}).then(() => {
